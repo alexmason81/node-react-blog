6c6005ee175f6d14298120a5db1597392021bbfe
Alex Mason <alex.mason@dewpoint.com>
2019-01-02 16:50:28 -0500

    adding initial client side code

---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../resources/scss/style.scss';
+
+ReactDOM.render(
+  <p>Hello World</p>,
+  document.getElementById('root'),
+);
